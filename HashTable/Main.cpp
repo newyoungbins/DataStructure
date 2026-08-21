763c2838226a0b83a87832aa65c723c6bcc0354c
newyoungbins <dudqlsjyb@naver.com>
2026-08-21 17:09:40 +0900

    해시테이블 진행

---
+++ HashTable/Main.cpp
@@ -0,0 +1,47 @@
+﻿#include "HashTable.h"
+#include <iostream>
+
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+int main()
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	HashTable table;
+
+	// 키-값 데이터 추가.
+	table.Add("Ronnie", "010-1234-5678");
+
+	// 중복 키 추가 확인.
+	if (table.Add("Ronnie", "010-243-2342"))
+	{
+		std::cout << "이미 같은 키가 저장돼 있음.\n";
+	}
+
+	table.Add("Kevin", "010-1234-1234");
+	table.Add("Baker", "010-1111-1234");
+	table.Add("YoungBin", "010-9876-5432");
+
+	// 출력.
+	std::cout << "\n전체 데이터\n";
+	table.Print();
+
+	// 검색.
+	std::string number;
+	if (table.Find("Ronnie", number))
+	{
+		std::cout << "\n검색 결과 : Ronnie, " << number << "\n";
+	}
+
+	// 삭제.
+	if (table.Delete("Baker"))
+	{
+		std::cout << "Baker 항목 삭제\n";
+	}
+
+	// 출력.
+	std::cout << "\n삭제 후 데이터\n";
+	table.Print();
+}
